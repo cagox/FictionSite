e9e1748b019641eddd336bc9be3a71e8b71b1bf2
burlingk <cagox@cagox.net>
2024-09-02 02:19:41 +0900

    Changed Rounding on Search Bar. Tiny commit. Just made the search box more rounded.

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -8,7 +8,7 @@ function PageHeader() {
         <div className="flex flex-row">
             <div className="basis-auto"><MenuButton /></div>
             <div className="basis-2/12"></div>
-            <div className="basis-8/12"><input type="text" className="size-full h-auto my-1 rounded-lg border text-lg pl-3" id="searchText" placeholder="Search Here"></input></div>
+            <div className="basis-8/12"><input type="text" className="size-full h-auto my-1 rounded-full border text-lg pl-3" id="searchText" placeholder="Search Here"></input></div>
             <div className="basis-auto"><SearchButton /></div>
             <div className="basis-2/12"></div>
             <div className="basis-auto"><UserButton /></div>
