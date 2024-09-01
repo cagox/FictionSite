1cbe1b48e3d011abd4a44a7ad8b52ce0b0f832b3
burlingk <cagox@cagox.net>
2024-09-02 01:30:30 +0900

    An initial push for this repo. I got the project set up with Vite, React, TypeScript and Tailwind. I have started the prototype for the front page, including some buttons.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import PageHeader from "./components/PageHeader"
+
+function App() {
+
+  return (
+    <div className="container mx-auto rounded-lg" id="appContainer">
+      <div className="grid">
+        <PageHeader />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,19 @@
+import UserButton from "./UserButton"
+import MenuButton from "./MenuButton"
+import SearchButton from "./SearchButton"
+
+
+function PageHeader() {
+    return(
+        <div className="flex flex-row">
+            <div className="basis-auto"><MenuButton /></div>
+            <div className="basis-2/12"></div>
+            <div className="basis-8/12"><input type="text" className="size-full h-auto my-1 rounded-lg border text-lg pl-3" id="searchText" placeholder="Search Here"></input></div>
+            <div className="basis-auto"><SearchButton /></div>
+            <div className="basis-2/12"></div>
+            <div className="basis-auto"><UserButton /></div>
+        </div>
+    )
+}
+
+export default PageHeader
