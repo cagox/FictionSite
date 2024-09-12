8fbbae445cbc82de1959d704cdcb4c794e1ab8a8
burlingk <cagox@cagox.net>
2024-09-12 23:40:11 +0900

    Created a basic log in form. This will be used to log in once the supporting code is in place.

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,6 +1,7 @@
-import UserButton from "./UserButton"
+/* import UserButton from "./UserButton" */
 import MenuButton from "./MenuButton"
 import SearchButton from "./SearchButton"
+import LoginMenu from "./LoginMenu"
 
 
 function PageHeader() {
@@ -8,10 +9,10 @@ function PageHeader() {
         <div className="flex flex-row">
             <div className="basis-auto"><MenuButton /></div>
             <div className="basis-2/12"></div>
-            <div className="basis-8/12"><input type="text" className="size-full h-auto my-1 rounded-full border text-lg pl-3" id="searchText" placeholder="Search Here"></input></div>
+            <div className="basis-full"><input type="text" className="size-full h-auto my-1 rounded-full border text-lg pl-3" id="searchText" placeholder="Search Here"></input></div>
             <div className="basis-auto"><SearchButton /></div>
             <div className="basis-2/12"></div>
-            <div className="basis-auto"><UserButton /></div>
+            <LoginMenu />
         </div>
     )
 }
