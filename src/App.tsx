cc4ae0753d20a8a2f5e5f9f1e9f87b739ac70819
burlingk <cagox@cagox.net>
2024-09-12 23:22:14 +0900

    Introduced the Layout component. I created the Layout component, and added it into App.tsx in preparation for adding in the router code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import PageFooter from "./components/PageFooter"
-import PageHeader from "./components/PageHeader"
+import Layout from "./components/Layout"
 
 function App() {
 
   return (
-    <div className="container mx-auto rounded-lg" id="appContainer">
-      <div className="grid">
-        <PageHeader />
-        <div className="min-h-96" id="contentArea">Content goes here.</div>
-        <PageFooter />
-      </div>
-    </div>
+    <Layout />
   )
 }
 
